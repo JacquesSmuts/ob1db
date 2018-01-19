b151f8e8b4b74fe3c937802b02d3d96ddad8105b
Jacques Smuts <git@jacquessmuts.com>
2018-01-19 21:23:46 +0200

    Data being stored in database, I think. Content Resolver not crashing, at least.

--- app/src/main/java/com/jacquessmuts/ob1db/activities/SplashActivity.kt
+++ app/src/main/java/com/jacquessmuts/ob1db/activities/SplashActivity.kt
@@ -5,9 +5,9 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import com.jacquessmuts.ob1db.R
+import com.jacquessmuts.ob1db.data.FilmContract
 import com.jacquessmuts.ob1db.models.Film
 import com.jacquessmuts.ob1db.network.NetworkClient
 import org.json.JSONObject
@@ -45,10 +45,24 @@ class SplashActivity : AppCompatActivity() {
             val filmsType = object : TypeToken<List<Film>>() {}.type
             val films = Gson().fromJson<List<Film>>(resultsArray, filmsType)
 
-            val filmValues : ContentValues;
-            //filmValues.put(FilmContract.FilmEntry.COLUMN_FILM_I)
+            val filmValuesArr = arrayOfNulls<ContentValues>(films.size)
+            for ((i, film) in films.withIndex()){
+                filmValuesArr[i] = ContentValues()
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_FILM_ID, film.episodeId)
+                //filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_CHARACTERS, film.characters)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_CREATED, film.created)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_DIRECTOR, film.director)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_EDITED, film.edited)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_OPENING_CRAWL, film.openingCrawl)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_PRODUCER, film.producer)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_RELEASE_DATE, film.releaseDate)
+//                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_STARSHIPS, film.starships)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_TITLE, film.title)
+                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_URL, film.url)
+//                filmValuesArr[i]!!.put(FilmContract.FilmEntry.COLUMN_VEHICLES, film.vehicles)
+            }
             val contentResolver = contentResolver;
-            //contentResolver.bulkInsert()
+            contentResolver.bulkInsert(FilmContract.FilmEntry.CONTENT_URI, filmValuesArr)
 
             //Log.e("Tag", result)
 
